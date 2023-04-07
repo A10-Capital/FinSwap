0b2a5aad6b2437c5d719d9ccb508ad54f33891d2
Sam B <sambarbosaa@gmail.com>
2023-04-07 00:45:49 +0000

    adedd script to provide liquidity, this script is feature complete now.

--- deploy.sh
+++ deploy.sh
@@ -5,36 +5,31 @@ set -e
 GAS="--gas-prices 0.25ukuji --gas auto --gas-adjustment 1.5 -y -b block"
 
 # Upload Pair Contract
-# CONTRACT=$(kujirad tx wasm store artifacts/terraswap_pair.wasm --from $KEY -y --broadcast-mode sync --output json $GAS | jq -r '.txhash') && echo $CONTRACT
-# sleep 2
-# PAIR_CODE_ID=$(kujirad query tx $CONTRACT --output json | jq -r '.logs[0].events[-1].attributes[-1].value') && echo "Pair code id: ${PAIR_CODE_ID}"
-# PAIR_CODE_ID=51
+CONTRACT=$(kujirad tx wasm store artifacts/terraswap_pair.wasm --from $KEY -y --broadcast-mode sync --output json $GAS | jq -r '.txhash') && echo $CONTRACT
+sleep 2
+PAIR_CODE_ID=$(kujirad query tx $CONTRACT --output json | jq -r '.logs[0].events[-1].attributes[-1].value') && echo "Pair code id: ${PAIR_CODE_ID}"
 
 # Upload Token Contract
-# CONTRACT=$(kujirad tx wasm store artifacts/terraswap_token.wasm --from $KEY -y --broadcast-mode sync --output json $GAS | jq -r '.txhash') && echo $CONTRACT
-# sleep 2
-# TOKEN_CODE_ID=$(kujirad query tx $CONTRACT --output json | jq -r '.logs[0].events[-1].attributes[-1].value') && echo "Token code id: ${TOKEN_CODE_ID}"
-# TOKEN_CODE_ID=52
+CONTRACT=$(kujirad tx wasm store artifacts/terraswap_token.wasm --from $KEY -y --broadcast-mode sync --output json $GAS | jq -r '.txhash') && echo $CONTRACT
+sleep 2
+TOKEN_CODE_ID=$(kujirad query tx $CONTRACT --output json | jq -r '.logs[0].events[-1].attributes[-1].value') && echo "Token code id: ${TOKEN_CODE_ID}"
 
 # Upload Factory Contract
-# CONTRACT=$(kujirad tx wasm store artifacts/terraswap_factory.wasm --from $KEY -y --broadcast-mode sync --output json $GAS | jq -r '.txhash') && echo $CONTRACT
-# sleep 2
-# FACTORY_CODE_ID=$(kujirad query tx $CONTRACT --output json | jq -r '.logs[0].events[-1].attributes[-1].value') && echo "Factory code id: ${FACTORY_CODE_ID}"
-# FACTORY_CODE_ID=53
+CONTRACT=$(kujirad tx wasm store artifacts/terraswap_factory.wasm --from $KEY -y --broadcast-mode sync --output json $GAS | jq -r '.txhash') && echo $CONTRACT
+sleep 2
+FACTORY_CODE_ID=$(kujirad query tx $CONTRACT --output json | jq -r '.logs[0].events[-1].attributes[-1].value') && echo "Factory code id: ${FACTORY_CODE_ID}"
 
 # Upload Router Contract
-# CONTRACT=$(kujirad tx wasm store artifacts/terraswap_router.wasm --from $KEY -y --broadcast-mode sync --output json $GAS | jq -r '.txhash') && echo $CONTRACT
-# sleep 2
-# ROUTER_CODE_ID=$(kujirad query tx $CONTRACT --output json | jq -r '.logs[0].events[-1].attributes[-1].value') && echo "Router code id: ${ROUTER_CODE_ID}"
-# ROUTER_CODE_ID=54
+CONTRACT=$(kujirad tx wasm store artifacts/terraswap_router.wasm --from $KEY -y --broadcast-mode sync --output json $GAS | jq -r '.txhash') && echo $CONTRACT
+sleep 2
+ROUTER_CODE_ID=$(kujirad query tx $CONTRACT --output json | jq -r '.logs[0].events[-1].attributes[-1].value') && echo "Router code id: ${ROUTER_CODE_ID}"
 
 # Instantiate Factory Contract
-# INIT_MSG="'{\"pair_code_id\": ${PAIR_CODE_ID}, \"token_code_id\": ${TOKEN_CODE_ID}}'"
-# MSG="kujirad tx wasm instantiate ${FACTORY_CODE_ID} ${INIT_MSG} --label 'Factory Contract' ${GAS} --broadcast-mode sync --output json -y --admin ${ADMIN} --from ${KEY}"
-# TX_INIT=$(eval $MSG | jq -r '.txhash' && echo)
-# sleep 3
-# ADDR=$(kujirad query tx $TX_INIT --output json | jq -r '.logs[0].events[0].attributes[0].value') && echo "FinSwap Factory Contract Address: $ADDR"
-ADDR="kujira1jlklduw85x8hj5rdsgk7easun84ypqca84tayt65fysl6vgkrcgqktz57x"
+INIT_MSG="'{\"pair_code_id\": ${PAIR_CODE_ID}, \"token_code_id\": ${TOKEN_CODE_ID}}'"
+MSG="kujirad tx wasm instantiate ${FACTORY_CODE_ID} ${INIT_MSG} --label 'Factory Contract' ${GAS} --broadcast-mode sync --output json -y --admin ${ADMIN} --from ${KEY}"
+TX_INIT=$(eval $MSG | jq -r '.txhash' && echo)
+sleep 3
+ADDR=$(kujirad query tx $TX_INIT --output json | jq -r '.logs[0].events[0].attributes[0].value') && echo "FinSwap Factory Contract Address: $ADDR"
 
 # Create New Native Denom and mint 1 Million to Self
 DENOM_RESULT=$(kujirad tx denom create-denom $DENOM $GAS --from $KEY --output json | jq -r '.txhash') && echo "Denom: $DENOM_RESULT"
@@ -65,5 +60,8 @@ CREATE_PAIR_MSG="kujirad tx wasm execute ${ADDR} ${ASSET_INFOS} $GAS --from $KEY
 echo "Create Pair: $CREATE_PAIR_MSG"
 CREATE_PAIR_RESULT=$(eval $CREATE_PAIR_MSG | jq -r '.txhash') && echo "Create Pair: $CREATE_PAIR_RESULT"
 sleep 1
-PAIR_ADDR=$(kujirad query tx $CREATE_PAIR_RESULT --output json | jq -r '.logs[0].events[0].attributes[0].value') && echo "Pair Address: $PAIR_ADDR"
-# Fund Pair
+PAIR_ADDR=$(kujirad query tx $CREATE_PAIR_RESULT --output json | jq -r '.logs[0].events[] | select(.type == "wasm") | .attributes[] | select(.key == "pair_contract_addr") | .value') && echo "Pair Address: $PAIR_ADDR"
+# Provide Liquidity to Pair
+PROVIDE_LIQ_MSG="'{\"provide_liquidity\":{\"assets\":[{\"info\":{\"native_token\":{\"denom\":\"ukuji\"}},\"amount\":\"50000000000\"},{\"info\":{\"native_token\":{\"denom\":\"${DENOM}\"}},\"amount\":\"500000000000\"}]}}'"
+MSG="kujirad tx wasm execute $PAIR_ADDR $PROVIDE_LIQ_MSG $GAS --from $KEY --output json --amount 50000000000ukuji,500000000000${DENOM}"
+PROVIDE_LIQ_RESULT=$(eval $MSG | jq -r '.txhash') && echo "Provide Liquidity: $PROVIDE_LIQ_RESULT"
